8fcfe2bbe095425e2bf7afd51fc8c3046a165a53
Olivier Laviale <contact@weirdog.com>
2013-04-29 18:07:27 +0200

    Initial commit

---
+++ descriptor.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Icybee\Modules\Articles;
+
+use ICanBoogie\Module;
+use ICanBoogie\ActiveRecord\Model;
+
+return array
+(
+	Module::T_CATEGORY => 'contents',
+	Module::T_DESCRIPTION => 'Introduces the Article content type.',
+	Module::T_EXTENDS => 'contents',
+	Module::T_MODELS => array
+	(
+		'primary' => array
+		(
+			Model::T_EXTENDS => 'contents'
+		)
+	),
+
+	Module::T_NAMESPACE => __NAMESPACE__,
+	Module::T_TITLE => 'Articles',
+	Module::T_VERSION => '1.0'
+);
